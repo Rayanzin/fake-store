865a517d492a2bc3eb097181c0eebab7517688e0
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-08-30 17:20:07 -0300

    projeto completo

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
-let url = 'https://fakestoreapi.com/products?limit=20'
+let url = 'https://fakestoreapi.com/products' 
+// 'https://fakestoreapi.com/products?limit=20'
 fetch(url)
-.then(function (response) { response
-.json().then(function (data) {
-    // console.log(data);
-        listarProdutos(data)
+    .then(function (response) {
+        response
+            .json().then(function (data) {
+                listarProdutos(data)
+            })
     })
-})
-
-function listarProdutos(dados){
-    for(let i = 0; i <= dados.length-1; i++){
-        cards.innerHTML += 
+let objeto;
+function listarProdutos(dados) {
+    objeto = dados
+    for (let i = 0; i <= dados.length - 1; i++) {
+        cards.innerHTML +=
+            `
+       <li>
+            <div>
+                <img src="${dados[i].image}">
+            </div>
+            <h2>${dados[i].title}</h2>
+            <p>Avaliação: ${dados[i].rating.rate}</p>
+            <p class="category">Categoria: ${dados[i].category}</p>
+        </li>
         `
-        <div>
-            <img src="${dados[i].image}">
-        </div>
-        <h2>${dados[i].title}</h2>
-        <p>${dados[i].description}</p>
-        `
+    }
+    // <p>${dados[i].description}</p>
+}
+li = document.getElementsByTagName('li')
+function filtrar() {
+    let pes = pesquisar.value 
+    for (let i = 0; i < objeto.length; i++){
+        if (objeto[i].title.toUpperCase().search(pes.toUpperCase()) != -1) {
+            li[i].style.display = 'block'
+        } else {
+            li[i].style.display = 'none'
+        }
+    }
+    if(pes == ''){
+        category()
+    }
+        
+}
+function category() {
+    for (let i = 0; i < objeto.length; i++) {
+        if (objeto[i].category == categoria.value || categoria.value == 'todos') {
+            li[i].style.display = 'block'
+        } else {
+            li[i].style.display = 'none'
+        }
+    }
+}
+function filtroPorAvaliacao() {
+    li.innerHTML = ''
+    for (let i = 0; i < objeto.length; i++) {
+        if (objeto[i].rating.rate >= avaliacao.value && categoria.value == 'todos' || objeto[i].rating.rate >= avaliacao.value && objeto[i].category == categoria.value) {
+            li[i].style.display = 'block'
+        } else if(avaliacao.value  == 'todos'){
+            category()
+        }else{
+            li[i].style.display = 'none'
+        }
     }
 }
